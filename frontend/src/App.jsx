ecf8d7287d252b19ddc35b6c6ffc7b7b682810ef
Junior Ledesma <juniorledesmaok@gmail.com>
2026-07-06 01:23:58 -0300

    Se realizaron los cambios solicitados

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://is3-demo-ci-cd-gaqv.onrender.com/";
+const API_URL = import.meta.env.VITE_API_URL || "https://is3-demo-ci-cd-gaqv.onrender.com";
 
 const formatARS = (value) =>
   new Intl.NumberFormat("es-AR", {
